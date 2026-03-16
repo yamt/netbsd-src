fd23652cd9b33ee1ccd628009b04bdbbf9de8fae
YAMAMOTO Takashi <imuwoto@gmail.com>
2026-03-16 13:53:17 +0000

    Revert "uvmexp nswget/pgswapin"
    This reverts commit b8fdb8f7737f6d3e488bdde10ee93a968b4340fb.

--- usr.bin/vmstat/vmstat.c
+++ usr.bin/vmstat/vmstat.c
@@ -929,9 +929,10 @@ dosum(void)
 		COPY(nswapdev);
 		COPY(swpages);
 		COPY(swpginuse);
-		COPY(pgswapin);
+		COPY(nswget);
 		COPY(pageins);
 		COPY(pdpageouts);
+		COPY(pgswapin);
 		COPY(pgswapout);
 		COPY(forks);
 		COPY(forks_ppwait);
@@ -1014,6 +1015,7 @@ dosum(void)
 	(void)printf("%9" PRIu64 " swap devices\n", uvmexp.nswapdev);
 	(void)printf("%9" PRIu64 " swap pages\n", uvmexp.swpages);
 	(void)printf("%9" PRIu64 " swap pages in use\n", uvmexp.swpginuse);
+	(void)printf("%9" PRIu64 " swap allocations\n", uvmexp.nswget);
 
 	cpucounters(&cc);
 
